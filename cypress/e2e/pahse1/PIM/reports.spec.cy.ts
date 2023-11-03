1b8ec01e96eb997528eb9fb8e90b6073c92fcf33
aya-thafer2 <ayathafer2@gmail.com>
2023-11-03 16:49:24 +0200

    Feature: create `reports.spec`

---
+++ cypress/e2e/pahse1/PIM/reports.spec.cy.ts
@@ -0,0 +1,10 @@
+describe('PIM: Reports', () => {
+
+    beforeEach(() => {
+        
+    })
+
+    it('PIM - Reports : Generate an Employee report with search criteria by (Personal : First name/ Job: Job title/ Salary:Amount)', () => {
+        
+    })
+})

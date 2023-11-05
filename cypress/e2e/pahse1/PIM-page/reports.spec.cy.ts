da7974c9296f1090815d09fd03fa798a9264d568
aya-thafer2 <ayathafer2@gmail.com>
2023-11-05 16:42:18 +0200

    Feature { Finish PreRequisites }

---
+++ cypress/e2e/pahse1/PIM-page/reports.spec.cy.ts
@@ -0,0 +1,32 @@
+import AddEmployeeDialogHelper from '../../../support/helpers/PIM-page/add-employee-dialog/add-employee-dialog-helper'
+import JobDialogHelper from '../../../support/helpers/admin-page/job-dialog/job-dialog-helper'
+import LocationDialogHelper from '../../../support/helpers/admin-page/location-dialog/location-dialog-helper'
+import GenericHelper from '../../../support/helpers/common-helpers/generic-helpers/generic-helper'
+
+describe('PIM: Reports', () => {
+
+    beforeEach(() => {
+        //admin logs-in to the system
+        GenericHelper.adminLogin()
+
+        //create 1 location  
+        LocationDialogHelper.createLocation();
+
+        //create 1 jobTitle 
+        JobDialogHelper.createJobTitle();
+
+        //create 3 employees
+        AddEmployeeDialogHelper.addEmployees(3)
+    })
+
+    afterEach(()=>{
+        //delete employee
+        //delete location
+        //delete jobTitle
+        //delete salary
+    })
+
+    it('PIM - Reports : Generate an Employee report with search criteria by (Personal : First name/ Job: Job title/ Salary:Amount)', () => {
+
+    })
+})

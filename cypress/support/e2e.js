ec94f4798cc1b7c485843bb5bc2bd57d8d79ec13
aya-thafer2 <ayathafer2@gmail.com>
2023-11-05 16:40:39 +0200

    Import **Custom Command** utils

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import '../support/utils/api-utils'
+import '../support/utils/ui-utils'
 import 'cypress-plugin-api'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
